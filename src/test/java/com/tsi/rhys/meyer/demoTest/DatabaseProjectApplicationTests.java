56269dab98601db1ae58f4619d1a27875d96613f
RhysMeyer <rhys.a.meyer@gmail.com>
2022-02-10 11:22:18 +0000

    ammeneded test

--- src/test/java/com/tsi/rhys/meyer/demoTest/DatabaseProjectApplicationTests.java
+++ src/test/java/com/tsi/rhys/meyer/demoTest/DatabaseProjectApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class DatabaseProjectApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
