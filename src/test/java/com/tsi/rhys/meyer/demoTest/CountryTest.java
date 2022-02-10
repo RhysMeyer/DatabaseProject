a274a10d687a87639a6ff6aa143e1efa6156b4a4
RhysMeyer <rhys.a.meyer@gmail.com>
2022-02-10 11:40:49 +0000

    added coverage to country tests

--- src/test/java/com/tsi/rhys/meyer/demoTest/CountryTest.java
+++ src/test/java/com/tsi/rhys/meyer/demoTest/CountryTest.java
@@ -3,6 +3,10 @@ package com.tsi.rhys.meyer.demoTest;
 import com.tsi.rhys.meyer.DatabaseProject.Country;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.sql.Timestamp;
+import java.util.Date;
+
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
@@ -18,18 +22,22 @@ public class CountryTest {
     @Test
     public void test_getName(){
         country.setCountry("TestCountry");
-        assertEquals("TestCountry",country.getCountry());
+        assertEquals("TestCountry", country.getCountry());
     }
 
-    @SpringBootTest
-    static
-    class SakilaMicroserviceApplicationTests {
+    public void test_getID(){
+        country.setCountry_id(100);
+        assertEquals(100,country.getCountry_id());
+    }
 
-        //	@Test
-        //	void contextLoads() {
-        //	}
+    public void test_setTimeStamp(){
+        Date date = new Date();
+        Timestamp ts = new Timestamp(date.getTime());
+        country.setLast_update(ts);
+        assertEquals(ts,country.getLast_update());
 
     }
+
 
 
 }
